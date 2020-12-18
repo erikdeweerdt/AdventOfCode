b58c50011508e5cb2664a4587466cc5632ad7735
Erik De Weerdt <eriksken@gmail.com>
2020-12-18 19:05:40 +0100

    day 18 part 2

--- 2020/18.py
+++ 2020/18.py
@@ -1,5 +1,5 @@
 import re
-from typing import Any,List
+from typing import Any, List
 
 
 def evaluate(expr: List[Any]) -> int:
@@ -24,6 +24,30 @@ def evaluate(expr: List[Any]) -> int:
     return result
 
 
+def evaluate_with_precedence(expr: List[Any]) -> int:
+    result = expr
+    try:
+        while True:
+            try:
+                index = result.index("+")
+                def oper(a, b): return a+b
+            except ValueError:
+                index = result.index("*")
+                def oper(a, b): return a*b
+            left = result[index-1]
+            right = result[index+1]
+            if isinstance(left, list):
+                left = evaluate_with_precedence(left)
+            if isinstance(right, list):
+                right = evaluate_with_precedence(right)
+            result[index] = oper(int(left), int(right))
+            del result[index+1]
+            del result[index-1]
+    except ValueError:
+        pass
+    return result[0]
+
+
 def process_parens(expr: List[str], index=0):
     result = []
     while index < len(expr):
@@ -46,15 +70,24 @@ def to_expr(line: str) -> List[str]:
 def part1(data):
     return sum([evaluate(to_expr(line)) for line in data if line])
 
+def part2(data):
+    return sum([evaluate_with_precedence(to_expr(line)) for line in data if line])
+
 
 if __name__ == "__main__":
     with open("data/18.txt") as f:
         data = f.read()
     data = data.splitlines()
-    print(evaluate(to_expr("1 + (2 * 3) + (4 * (5 + 6))")))
-    print(evaluate(to_expr("2 * 3 + (4 * 5)")))
-    print(evaluate(to_expr("5 + (8 * 3 + 9 + 3 * 4 * 3)")))
-    print(evaluate(to_expr("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")))
-    print(evaluate(to_expr("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")))
-    # print(process_parens(to_expr("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"))[1])
+    # print(evaluate(to_expr("1 + (2 * 3) + (4 * (5 + 6))")))
+    # print(evaluate(to_expr("2 * 3 + (4 * 5)")))
+    # print(evaluate(to_expr("5 + (8 * 3 + 9 + 3 * 4 * 3)")))
+    # print(evaluate(to_expr("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")))
+    # print(evaluate(to_expr("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")))
+    # print("---------")
+    # print(evaluate_with_precedence(to_expr("1 + (2 * 3) + (4 * (5 + 6))")))
+    # print(evaluate_with_precedence(to_expr("2 * 3 + (4 * 5)")))
+    # print(evaluate_with_precedence(to_expr("5 + (8 * 3 + 9 + 3 * 4 * 3)")))
+    # print(evaluate_with_precedence(to_expr("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")))
+    # print(evaluate_with_precedence(to_expr("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")))
     # print(part1(data))
+    print(part2(data))
