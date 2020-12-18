e5fc0ba03b1d214743c6cf3c0d84483fc07ac05e
Erik De Weerdt <eriksken@gmail.com>
2020-12-18 12:39:29 +0100

    day 16 part 2

--- 2020/16.py
+++ 2020/16.py
@@ -43,6 +43,26 @@ def get_matching_classes(value, rules):
                 break
 
 
+def solve(ticket_classes):
+    # compress all tickets into a single vector to solve
+    classes = ticket_classes[0]
+    for ticket in ticket_classes[1:]:
+        for i in range(len(ticket)):
+            classes[i] = [c for c in classes[i] if c in ticket[i]]
+    # solve by repeatedly removing slots with only one possibility from every other slot
+    candidate = next((i for i in range(len(classes)) if len(classes[i]) == 1 and isinstance(classes[i], list)), None)
+    while candidate is not None:
+        name = classes[candidate][0]
+        classes[candidate] = name
+        candidate = None
+        for i in range(len(classes)):
+            if isinstance(classes[i], list):
+                classes[i].remove(name)
+                if len(classes[i]) == 1:
+                    candidate = i
+    return classes
+
+
 def part1(data):
     rules = parse_rules(data)
     your_ticket, *nearby_tickets = parse_tickets(data)
@@ -56,8 +76,25 @@ def part1(data):
     print(error_rate)
 
 
+def part2(data):
+    rules = parse_rules(data)
+    your_ticket, *nearby_tickets = parse_tickets(data)
+    # get all possible classes for every position, filtering out invalid (some value matches nothing) tickets
+    classes = list(filter(lambda l: [] not in l,
+                          ([list(get_matching_classes(value, rules)) for value in ticket] for ticket in nearby_tickets)))
+    classes = solve(ticket_classes=classes)
+    print(your_ticket)
+    print(classes)
+    result = 1
+    for i in range(len(classes)):
+        if classes[i].startswith("departure"):
+            result *= your_ticket[i]
+    print(result)
+
+
 if __name__ == "__main__":
     with open("data/16.txt") as f:
         data = f.read()
     data = data.splitlines()
-    part1(data)
+    # part1(data)
+    part2(data)
