c9b3f529169b304c408807353c70cdd389a0f92a
Erik De Weerdt <eriksken@gmail.com>
2020-12-18 16:49:02 +0100

    day 17 part 2

--- 2020/17.py
+++ 2020/17.py
@@ -18,16 +18,17 @@ DATA = """
 
 
 class Cube:
-    def __init__(self, x: int, y: int, z: int, active=False):
+    def __init__(self, x: int, y: int, z: int, w: int, active=False):
         self.x = x
         self.y = y
         self.z = z
+        self.w = w
         self.active = active
         self.next = active
 
     def __str__(self):
         token = "#" if self.active else "."
-        return f"({self.x},{self.y},{self.z})[{token}]"
+        return f"({self.x},{self.y},{self.z},{self.w})[{token}]"
 
     def __repr__(self):
         return self.__str__()
@@ -35,7 +36,7 @@ class Cube:
 
 class PocketDimension:
     cubes: List[Cube]
-    lookup: Dict[int, Dict[int, Dict[int, Cube]]]
+    lookup: Dict[int, Dict[int, Dict[int, Dict[int, Cube]]]]
 
     def __init__(self, cubes: List[Cube]):
         self.cubes = cubes
@@ -49,32 +50,35 @@ class PocketDimension:
                 self.lookup[cube.x] = {}
             if cube.y not in self.lookup[cube.x]:
                 self.lookup[cube.x][cube.y] = {}
-            self.lookup[cube.x][cube.y][cube.z] = cube
+            if cube.z not in self.lookup[cube.x][cube.y]:
+                self.lookup[cube.x][cube.y][cube.z] = {}
+            self.lookup[cube.x][cube.y][cube.z][cube.w] = cube
 
     def _ensure_neighbors(self) -> None:
         for cube in self.cubes:
             if cube.active:
-                for x, y, z in PocketDimension._neighbor_range(cube):
+                for x, y, z, w in PocketDimension._neighbor_range(cube):
                     try:
-                        _ = self.lookup[x][y][z]
+                        _ = self.lookup[x][y][z][w]
                     except KeyError:
-                        new_cube = Cube(x, y, z)
+                        new_cube = Cube(x, y, z, w)
                         self.cubes.append(new_cube)
                         self._insert_cubes([new_cube])
 
     @staticmethod
-    def _neighbor_range(cube) -> Iterable[Tuple[int, int, int]]:
+    def _neighbor_range(cube) -> Iterable[Tuple[int, int, int, int]]:
         for x in range(cube.x-1, cube.x+2):
             for y in range(cube.y-1, cube.y+2):
                 for z in range(cube.z-1, cube.z+2):
-                    if (cube.x != x or cube.y != y or cube.z != z):
-                        yield (x, y, z)
+                    for w in range(cube.w-1, cube.w+2):
+                        if (cube.x != x or cube.y != y or cube.z != z or cube.w != w):
+                            yield (x, y, z, w)
 
     def active_neighbors(self, cube) -> int:
         active = 0
-        for x, y, z in PocketDimension._neighbor_range(cube):
+        for x, y, z, w in PocketDimension._neighbor_range(cube):
             try:
-                if self.lookup[x][y][z].active:
+                if self.lookup[x][y][z][w].active:
                     active += 1
             except KeyError:
                 pass
@@ -87,8 +91,6 @@ class PocketDimension:
                 cube.next = active == 2 or active == 3
             else:
                 cube.next = active == 3
-            # if cube.z == 0:
-            #     print(f"({cube.x},{cube.y},{cube.z}): {active} {cube.active} -> {cube.next}")
         for cube in self.cubes:
             cube.active = cube.next
         self._ensure_neighbors()
@@ -101,13 +103,13 @@ def make_dimension(data: str) -> PocketDimension:
         if line:
             x = 0
             for token in line:
-                cubes.append(Cube(x, y, 0, token == "#"))
+                cubes.append(Cube(x, y, 0, 0, token == "#"))
                 x += 1
             y += 1
     return PocketDimension(cubes)
 
 
-def part1(data: str):
+def main(data: str):
     dimension = make_dimension(data)
     for _ in range(6):
         dimension.step()
@@ -116,4 +118,5 @@ def part1(data: str):
 
 
 if __name__ == "__main__":
-    part1(DATA)
+    # part 1 is without the w dimension
+    main(DATA)
