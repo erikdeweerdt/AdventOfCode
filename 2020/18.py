3263462b17e990553b3fae29502c94f6067dd553
Erik De Weerdt <eriksken@gmail.com>
2020-12-18 18:45:28 +0100

    simplify evaluate

--- 2020/18.py
+++ 2020/18.py
@@ -1,24 +1,18 @@
 import re
-from typing import List
+from typing import Any,List
 
 
-def evaluate(expr: List[str], index: int = 0) -> int:
+def evaluate(expr: List[Any]) -> int:
     result = None
     operator = None
-    # print(f">> [{index}]")
-    while index < len(expr):
-        token = expr[index]
-        index += 1
+    for subexpr in expr:
         operand = None
-        if token == "+" or token == "*":
-            operator = token
-        elif token == "(":
-            index, operand = evaluate(expr, index)
-        elif token == ")":
-            break
+        if isinstance(subexpr, list):
+            operand = evaluate(subexpr)
+        elif subexpr == "+" or subexpr == "*":
+            operator = subexpr
         else:
-            operand = int(token)
-        # print(result, operator, operand)
+            operand = int(subexpr)
         if operand is not None:
             if operator == "+":
                 result += operand
@@ -27,21 +21,40 @@ def evaluate(expr: List[str], index: int = 0) -> int:
             else:
                 result = operand
     # print(f"<< {result}")
+    return result
+
+
+def process_parens(expr: List[str], index=0):
+    result = []
+    while index < len(expr):
+        token = expr[index]
+        index += 1
+        if token == "(":
+            index, subexpr = process_parens(expr, index)
+            result.append(subexpr)
+        elif token == ")":
+            break
+        else:
+            result.append(token)
     return (index, result)
 
 
 def to_expr(line: str) -> List[str]:
-    return re.findall(r"(\d+|\(|\)|\+|\*)", line)
+    return process_parens(re.findall(r"(\d+|\(|\)|\+|\*)", line))[1]
+
 
 def part1(data):
-    return sum([evaluate(to_expr(line))[1] for line in data if line])
+    return sum([evaluate(to_expr(line)) for line in data if line])
+
 
 if __name__ == "__main__":
     with open("data/18.txt") as f:
         data = f.read()
     data = data.splitlines()
-    # print(evaluate(to_expr("2 * 3 + (4 * 5)")))
-    # print(evaluate(to_expr("5 + (8 * 3 + 9 + 3 * 4 * 3)")))
-    # print(evaluate(to_expr("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")))
-    # print(evaluate(to_expr("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")))
-    print(part1(data))
+    print(evaluate(to_expr("1 + (2 * 3) + (4 * (5 + 6))")))
+    print(evaluate(to_expr("2 * 3 + (4 * 5)")))
+    print(evaluate(to_expr("5 + (8 * 3 + 9 + 3 * 4 * 3)")))
+    print(evaluate(to_expr("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")))
+    print(evaluate(to_expr("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")))
+    # print(process_parens(to_expr("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"))[1])
+    # print(part1(data))
