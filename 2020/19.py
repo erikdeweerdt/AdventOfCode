7c1d0f354bef31d33ef67d497481516683cdba47
Erik De Weerdt <eriksken@gmail.com>
2020-12-19 12:58:39 +0100

    update parse_rules for missing indexes

--- 2020/19.py
+++ 2020/19.py
@@ -15,6 +15,55 @@ abbbab
 aaabbb
 aaaabbb
 """.splitlines()
+TESTDATA2 = """
+42: 9 14 | 10 1
+9: 14 27 | 1 26
+10: 23 14 | 28 1
+1: "a"
+11: 42 31
+5: 1 14 | 15 1
+19: 14 1 | 14 14
+12: 24 14 | 19 1
+16: 15 1 | 14 14
+31: 14 17 | 1 13
+6: 14 14 | 1 14
+2: 1 24 | 14 4
+0: 8 11
+13: 14 3 | 1 12
+15: 1 | 14
+17: 14 2 | 1 7
+23: 25 1 | 22 14
+28: 16 1
+4: 1 1
+20: 14 14 | 1 15
+3: 5 14 | 16 1
+27: 1 6 | 14 18
+14: "b"
+21: 14 1 | 1 14
+25: 1 1 | 1 14
+22: 14 14
+8: 42
+26: 14 22 | 1 20
+18: 15 15
+7: 14 5 | 1 21
+24: 14 1
+
+abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+bbabbbbaabaabba
+babbbbaabbbbbabbbbbbaabaaabaaa
+aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+bbbbbbbaaaabbbbaaabbabaaa
+bbbababbbbaaaaaaaabbababaaababaabab
+ababaaaaaabaaab
+ababaaaaabbbaba
+baabbaaaabbaaaababbaababb
+abbbbabbbbaaaababbbbbbaaaababb
+aaaaabbaabaaaaababaa
+aaaabbaaaabbaaa
+aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+babaaabbbaaabaababbaabababaaab
+aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba
+""".splitlines()
 
 
 def compile_pattern(rules: List[str], index=0) -> str:
@@ -32,13 +81,16 @@ def compile_pattern(rules: List[str], index=0) -> str:
 
 def parse_rules(data: List[str]) -> List[str]:
     # rules in the input are not ordered
-    # collect them first and return a list by sorting the keys
+    # some rules numbers may not be defined
     rules = {}
+    max_index = 0
     for line in data:
         match = re.match(r"^(\d+): (.*)", line)
         if match:
-            rules[int(match.group(1))] = match.group(2)
-    return [rules[index] for index in sorted(rules.keys())]
+            index = int(match.group(1))
+            rules[index] = match.group(2)
+            max_index = index if index > max_index else max_index
+    return [rules.pop(index, None) for index in range(max_index + 1)]
 
 
 def parse_messages(data: List[str]) -> List[str]:
@@ -56,8 +108,13 @@ def part1(data):
     print(sum(1 if regex.fullmatch(message) else 0 for message in messages))
 
 
+def part2(data):
+    part1(data)
+
+
 if __name__ == "__main__":
     with open("data/19.txt") as f:
         data = f.read()
     data = data.splitlines()
-    part1(data)
+    # part1(data)
+    part2(TESTDATA2)
