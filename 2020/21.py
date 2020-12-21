078c43324605c60e8a14d4584251fb0a33f82f0a
Erik De Weerdt <eriksken@gmail.com>
2020-12-21 11:05:10 +0100

    day 21 part 2

--- 2020/21.py
+++ 2020/21.py
@@ -41,6 +41,22 @@ def reduce_allergens(allergens: Dict[str, List[List[str]]]) -> Dict[str, Set[str
     return reduced
 
 
+def solve(allergens: Dict[str, Set[str]]) -> Dict[str, Set[str]]:
+    solution = {key: allergens[key].copy() for key in allergens}
+    keys = set(solution.keys())
+    try:
+        while True:
+            candidate = next(key for key in keys if len(solution[key]) == 1)
+            food = next(iter(solution[candidate]))
+            for key, foods in solution.items():
+                if key != candidate and food in foods:
+                    foods.remove(food)
+            keys.remove(candidate)
+    except StopIteration:
+        pass
+    return solution
+
+
 def part1(data: List[str]):
     recipes, foods, allergens = parse_foods(data)
     reduced = reduce_allergens(allergens)
@@ -52,9 +68,17 @@ def part1(data: List[str]):
     print(sum(1 for recipe in recipes for _ in allergen_free.intersection(recipe)))
 
 
+def part2(data: List[str]):
+    *_, allergens = parse_foods(data)
+    reduced = reduce_allergens(allergens)
+    solution = solve(reduced)
+    print(solution)
+    print(",".join(next(iter(solution[allergen])) for allergen in sorted(solution.keys())))
+
+
 if __name__ == "__main__":
     with open("data/21.txt") as f:
         data = f.read()
     data = data.splitlines()
-    part1(data)
-    # part2(data)
+    # part1(data)
+    part2(data)
