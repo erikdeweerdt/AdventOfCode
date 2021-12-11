bb16a81f0ee57035c1fb5d8acf635ae7b5875abf
Erik De Weerdt <git@mail.dwrdt.net>
2021-12-11 11:40:18 +0100

    2021.11 cleaned up

--- 2021/11.py
+++ 2021/11.py
@@ -18,30 +18,7 @@ def part1():
     # grid = [[int(c) for c in l] for l in TESTDATA]
     count = 0
     for _ in range(100):
-        # print_grid(grid)
-        # print('-' * 19)
-        # increase all by 1 and enqueue all > 9
-        q = []
-        for y in range(10):
-            for x in range(10):
-                grid[y][x] += 1
-                if grid[y][x] > 9:
-                    q.append((x, y))
-        # process flashers
-        flashes = []
-        while q:
-            qx, qy = q.pop(0)
-            flashes.append((qx, qy))
-            for nx, ny in neighbors(qx, qy):
-                if grid[ny][nx] <= 9:
-                    grid[ny][nx] += 1
-                    if grid[ny][nx] > 9:
-                        q.append((nx, ny))
-        # count and reset
-        # print(flashes)
-        count += len(flashes)
-        for x, y in flashes:
-            grid[y][x] = 0
+        count += flash(grid)
     print(count)
 
 
@@ -49,38 +26,33 @@ def part2():
     with open("data/11.txt") as f:
         grid = [[int(c) for c in l.strip()] for l in f.readlines() if l]
     # grid = [[int(c) for c in l] for l in TESTDATA]
-    count = 0
     i = 0
     while True:
         i += 1
-        # print_grid(grid)
-        # print('-' * 19)
-        # increase all by 1 and enqueue all > 9
-        q = []
-        for y in range(10):
-            for x in range(10):
-                grid[y][x] += 1
-                if grid[y][x] > 9:
-                    q.append((x, y))
-        # process flashers
-        flashes = []
-        while q:
-            qx, qy = q.pop(0)
-            flashes.append((qx, qy))
-            for nx, ny in neighbors(qx, qy):
-                if grid[ny][nx] <= 9:
-                    grid[ny][nx] += 1
-                    if grid[ny][nx] > 9:
-                        q.append((nx, ny))
-        # count and reset
-        # print(flashes)
-        if len(flashes) == 100:
-            print(i)
+        if flash(grid) == 100:
             break
-        count += len(flashes)
-        for x, y in flashes:
-            grid[y][x] = 0
-    # print(count)
+    print(i)
+
+
+def flash(grid):
+    q = []
+    for y in range(10):
+        for x in range(10):
+            grid[y][x] += 1
+            if grid[y][x] > 9:
+                q.append((x, y))
+    flashes = []
+    while q:
+        qx, qy = q.pop(0)
+        flashes.append((qx, qy))
+        for nx, ny in neighbors(qx, qy):
+            if grid[ny][nx] <= 9:
+                grid[ny][nx] += 1
+                if grid[ny][nx] > 9:
+                    q.append((nx, ny))
+    for x, y in flashes:
+        grid[y][x] = 0
+    return len(flashes)
 
 
 def print_grid(grid):
@@ -95,5 +67,5 @@ def neighbors(x, y):
 
 
 if __name__ == '__main__':
-    # part1()
+    part1()
     part2()
