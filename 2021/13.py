9dd8ae7de0fa64637390f98057ddceebdeefab31
Erik De Weerdt <git@mail.dwrdt.net>
2021-12-13 11:02:23 +0100

    2021.13

---
+++ 2021/13.py
@@ -0,0 +1,93 @@
+TESTDATA = [
+    '6,10',
+    '0,14',
+    '9,10',
+    '0,3',
+    '10,4',
+    '4,11',
+    '6,0',
+    '6,12',
+    '4,1',
+    '0,13',
+    '10,12',
+    '3,4',
+    '3,0',
+    '8,4',
+    '1,10',
+    '2,14',
+    '8,10',
+    '9,0',
+    '',
+    'fold along y=7',
+    'fold along x=5',
+]
+
+
+class Grid:
+    def __init__(self) -> None:
+        self.__grid = set()
+        self.__size = (0, 0)
+
+    def __str__(self):
+        return '\n'.join(' '.join('#' if (x, y) in self.__grid else '.' for x in range(
+            self.__size[0])) for y in range(self.__size[1]))
+
+    def add(self, x, y):
+        self.__grid.add((x, y))
+        self.__size = (max(self.__size[0], x + 1), max(self.__size[1], y + 1))
+
+    def fold(self, fold):
+        # partition left-right and merge
+        axis, value = fold
+        grid = set()
+        for p in self.__grid:
+            # immediately merge in points to the right
+            # no points are on the fold, so ignore that case
+            if p[axis] < value:
+                grid.add(p)
+            else:
+                np = list(p)
+                np[axis] = 2 * value - np[axis]
+                grid.add(tuple(np))
+        self.__grid = grid
+        s = list(self.__size)
+        s[axis] //= 2
+        self.__size = tuple(s)
+
+    def count_dots(self):
+        # return (self.__size[0]) * (self.__size[1]) - len(self.__grid)
+        return len(self.__grid)
+
+
+def part1():
+    grid, folds = read()
+    grid.fold(folds[0])
+    print(grid.count_dots())
+
+
+def part2():
+    grid, folds = read()
+    for f in folds:
+        grid.fold(f)
+    print(grid)
+
+
+def read():
+    with open("data/13.txt") as f:
+        data = [l.strip() for l in f.readlines() if l.strip()]
+    # data = [l.strip() for l in TESTDATA if l]
+    grid = Grid()
+    folds = []
+    for l in data:
+        if l.startswith('fold along '):
+            # x-axis = 0, y-axis = 1
+            folds.append((0 if l[11] == 'x' else 1, int(l[13:])))
+        else:
+            x, y = l.split(',')
+            grid.add(int(x), int(y))
+    return grid, folds
+
+
+if __name__ == '__main__':
+    # part1()
+    part2()
