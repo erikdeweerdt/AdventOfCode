de52fc566c4f1ededc5c66743f6df30ef97ca721
Erik De Weerdt <git@mail.dwrdt.net>
2021-12-21 18:22:11 +0100

    2021.21 part 2

--- 2021/21.py
+++ 2021/21.py
@@ -1,3 +1,8 @@
+from functools import reduce
+
+PROBABILITIES = [0, 0, 0, 1, 3, 6, 7, 6, 3, 1]
+
+
 def part1():
     # loser, rolls = deterministic(4, 8)
     loser, rolls = deterministic(5, 9)
@@ -5,13 +10,35 @@ def part1():
 
 
 def part2():
-    pass
+    # print(quantum(4 - 1, 8 - 1))
+    print(quantum(5 - 1, 9 - 1))
 
 
-def quantum(player1, player2):
-    # still roll 3 times
-    # -> every turn yields a sum of at least 3 and at most 9 (normally distributed)
-    pass
+# turn = True if player1 is playing
+# there may be a more clever way of doing this, but I don't care
+def quantum(p1, p2, s1=0, s2=0, u=1, turn=True):
+    if turn:
+        if s2 >= 21:
+            return (0, u)
+        u1, u2 = 0, 0
+        for die in range(3, 10):
+            p1n = (p1 + die) % 10
+            # u * PROBABILITIES[die] because we only consider the number of universes created by _this_ outcome
+            # all of them together should add up to 27
+            u1n, u2n = quantum(p1n, p2, s1 + p1n + 1, s2, u * PROBABILITIES[die], not turn)
+            u1 += u1n
+            u2 += u2n
+        return (u1, u2)
+    else:
+        if s1 >= 21:
+            return (u, 0)
+        u1, u2 = 0, 0
+        for die in range(3, 10):
+            p2n = (p2 + die) % 10
+            u1n, u2n = quantum(p1, p2n, s1, s2 + p2n + 1, u * PROBABILITIES[die], not turn)
+            u1 += u1n
+            u2 += u2n
+        return (u1, u2)
 
 
 def deterministic(player1, player2):
